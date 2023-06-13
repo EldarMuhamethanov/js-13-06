5c3426c6ff85dac93f08c24fe1c1247d6e42b992
EldarMuhamethanov <eldarmy@mail.ru>
2023-06-13 21:26:35 +0300

    Реализовал функцию fetchGracePeriodLogicFromMapping

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const _ = require('lodash')
 
+const DATA_SEPARATOR = ';'
+const DATA_VALUE_SEPARATOR = ':'
+const CARD_IDS_SEPARATOR = ','
+
+function parseMapping(mapping) {
+    const mappingWithoutSpaces = _.replace(mapping, /\s/g, '')
+    const cardIdToNameMap = new Map()
+    mappingWithoutSpaces.split(DATA_SEPARATOR).forEach(data => {
+        if (!data) {
+            return
+        }
+        const [dataName, cardIdsString] = data.split(DATA_VALUE_SEPARATOR)
+        if (!dataName || !cardIdsString) {
+            return;
+        }
+
+        const cardIds = cardIdsString.split(CARD_IDS_SEPARATOR)
+        cardIds.forEach(id => cardIdToNameMap.set(id, dataName))
+    })
+    return cardIdToNameMap
+}
+
 function fetchGracePeriodLogicFromMapping (cardId, mapping = '') {
-    return "base"
+    if (!mapping || !cardId) {
+        return null
+    }
+
+    const cardIdToNameMap = parseMapping(mapping)
+    return cardIdToNameMap.get(cardId) || null
 }
 
 module.exports = fetchGracePeriodLogicFromMapping
